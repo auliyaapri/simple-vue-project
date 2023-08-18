6c2155c86256124ac0fbe017d38049f9df367f42
auliyaapri <apri.wiguna29@gmail.com>
2023-08-18 09:14:02 +0700

    first commit

---
+++ src/components/Student.vue
@@ -0,0 +1,32 @@
+<template>
+        Hello, ini adalah komponen student {{ nameStudent }}         
+         <button @click="notification">Klik Detail Bro</button><br><br>
+
+         <slot>Ini adalah slot</slot>
+
+</template>
+
+<script >
+    export default {
+        props : {
+            nameStudent : String
+        },
+        emits: ['response'],
+        // methods: {
+        //     notification() { // Method notification akan dipanggil ketika tombol diklik.
+        //         this.$emit('show-alert', `detail untuk siswa ${this.nameStudent}`)
+        //         console.log(notification);
+        //     }
+        // },
+        methods: {
+            notification() { // Method notification akan dipanggil ketika tombol diklik.
+                this.$emit('response', this.nameStudent)
+                // console.log(this.$emit('response', this.nameStudent));
+            }
+        },
+        
+    }
+    
+</script>
+
+
